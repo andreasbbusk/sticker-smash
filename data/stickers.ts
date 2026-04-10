70178cf24f1520092344c957eedf9ad1d7cf2879
andreasbbusk <buskmikkelsen@icloud.com>
2026-04-10 10:44:19 +0200

    Add sticker data for dynamic routes

---
+++ data/stickers.ts
@@ -0,0 +1,51 @@
+import { ImageSourcePropType } from "react-native";
+
+export type Sticker = {
+  id: string;
+  name: string;
+  description: string;
+  image: ImageSourcePropType;
+};
+
+export const stickers: Sticker[] = [
+  {
+    id: "cool-kitten",
+    name: "Cool Kitten",
+    description: "A laid-back sticker that works well for fun, playful edits.",
+    image: require("../assets/images/emoji1.png") as ImageSourcePropType,
+  },
+  {
+    id: "laugh-attack",
+    name: "Laugh Attack",
+    description: "A cheerful sticker for photos that need a little extra energy.",
+    image: require("../assets/images/emoji2.png") as ImageSourcePropType,
+  },
+  {
+    id: "star-eyes",
+    name: "Star Eyes",
+    description: "A bright reaction sticker that makes favorite moments stand out.",
+    image: require("../assets/images/emoji3.png") as ImageSourcePropType,
+  },
+  {
+    id: "party-mood",
+    name: "Party Mood",
+    description: "A festive sticker for snapshots that deserve celebration.",
+    image: require("../assets/images/emoji4.png") as ImageSourcePropType,
+  },
+  {
+    id: "love-bug",
+    name: "Love Bug",
+    description: "A sweet sticker that fits portraits, pets, and feel-good photos.",
+    image: require("../assets/images/emoji5.png") as ImageSourcePropType,
+  },
+  {
+    id: "sunny-smile",
+    name: "Sunny Smile",
+    description: "A simple happy sticker for light, casual photo edits.",
+    image: require("../assets/images/emoji6.png") as ImageSourcePropType,
+  },
+];
+
+export function getStickerById(id: string) {
+  return stickers.find((sticker) => sticker.id === id);
+}
